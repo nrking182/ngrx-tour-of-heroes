d89d544ef23914b1f6ed8956c6dcc69b2f745bdc
Nick King <nrking182@gmail.com>
2021-07-06 10:34:03 -0500

    test commit

--- src/app/heroes/store/reducers/heroes.reducer.ts
+++ src/app/heroes/store/reducers/heroes.reducer.ts
@@ -1,93 +1,96 @@
-import * as fromHeroes from '../actions/heroes.actions';
-import { Hero } from '../../models/hero';
+import * as fromHeroes from "../actions/heroes.actions";
+import { Hero } from "../../models/hero";
 
+//test commit
 export interface HeroState {
-    data: Hero[];
-    loaded: boolean;
-    loading: boolean;
+  data: Hero[];
+  loaded: boolean;
+  loading: boolean;
 }
 
 export const initialState: HeroState = {
-    data: [{id: 45, name: 'dank'}],
-    loaded: false,
-    loading: false
-}
+  data: [{ id: 45, name: "dank" }],
+  loaded: false,
+  loading: false
+};
 
-export function reducer(state = initialState, action: fromHeroes.HeroesAction): HeroState {
-
-    switch(action.type){
-        case fromHeroes.LOAD_HEROES: {
-            return {
-                ...state,
-                loading: true,
-                loaded: false
-            }
-        }
-        case fromHeroes.LOAD_HEROES_SUCCESS: {
-            const data = action.payload;
-            return {
-                ...state,
-                loading: false,
-                loaded: true,
-                data
-            }
-        }
-        case fromHeroes.LOAD_HEROES_FAIL: {
-            return {
-                ...state,
-                loading: false,
-                loaded: true
-            }
-        }
-        case fromHeroes.ADD_HERO: {
-            return {
-                ...state,
-                loading: true,
-                loaded: false
-            }
-        }
-        case fromHeroes.ADD_HERO_SUCCESS: {
-            const data = action.payload;
-            return {
-                ...state,
-                data: [...state.data, {id: data.id, name: data.name}],
-                loading: false,
-                loaded: true,
-            }
-        }
-        case fromHeroes.ADD_HERO_FAIL: {
-            return {
-                ...state,
-                loading: false,
-                loaded: true
-            }
-        }
-        case fromHeroes.DELETE_HERO: {
-            return {
-                ...state,
-                loading: true,
-                loaded: false
-            }
-        }
-        case fromHeroes.DELETE_HERO_SUCCESS: {
-            const actionData = action.payload;
-            return {
-                ...state,
-                data: state.data.filter((hero) => hero !== action.payload),
-                loading: false,
-                loaded: true
-            }
-        }
-        case fromHeroes.DELETE_HERO_FAIL: {
-            return {
-                ...state,
-                loading: true,
-                loaded: false
-            }
-        }
+export function reducer(
+  state = initialState,
+  action: fromHeroes.HeroesAction
+): HeroState {
+  switch (action.type) {
+    case fromHeroes.LOAD_HEROES: {
+      return {
+        ...state,
+        loading: true,
+        loaded: false
+      };
     }
-    console.log('INIT STATE::::', state);
-    return state;
+    case fromHeroes.LOAD_HEROES_SUCCESS: {
+      const data = action.payload;
+      return {
+        ...state,
+        loading: false,
+        loaded: true,
+        data
+      };
+    }
+    case fromHeroes.LOAD_HEROES_FAIL: {
+      return {
+        ...state,
+        loading: false,
+        loaded: true
+      };
+    }
+    case fromHeroes.ADD_HERO: {
+      return {
+        ...state,
+        loading: true,
+        loaded: false
+      };
+    }
+    case fromHeroes.ADD_HERO_SUCCESS: {
+      const data = action.payload;
+      return {
+        ...state,
+        data: [...state.data, { id: data.id, name: data.name }],
+        loading: false,
+        loaded: true
+      };
+    }
+    case fromHeroes.ADD_HERO_FAIL: {
+      return {
+        ...state,
+        loading: false,
+        loaded: true
+      };
+    }
+    case fromHeroes.DELETE_HERO: {
+      return {
+        ...state,
+        loading: true,
+        loaded: false
+      };
+    }
+    case fromHeroes.DELETE_HERO_SUCCESS: {
+      const actionData = action.payload;
+      return {
+        ...state,
+        data: state.data.filter(hero => hero !== action.payload),
+        loading: false,
+        loaded: true
+      };
+    }
+    case fromHeroes.DELETE_HERO_FAIL: {
+      return {
+        ...state,
+        loading: true,
+        loaded: false
+      };
+    }
+  }
+  console.log("INIT STATE::::", state);
+  return state;
 }
 
 export const getHeroes = (state: HeroState) => state.data;
